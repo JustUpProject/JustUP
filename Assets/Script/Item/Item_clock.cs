2c2fb51359fcbfb3725798083a429805052258db
keonhwa <jk41587230@gmail.com>
2024-01-06 16:46:01 +0900

    01/06 지면생성 완성, 시계 시작
    지면생성 완성, 시계 시작

---
+++ Assets/Script/Item/Item_clock.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_clock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+/*public class TimeControl : MonoBehaviour
+{
+    void Update()
+    {
+        // 어떤 조건에 따라 시간을 느리게 할 수 있습니다.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 시간을 절반으로 줄입니다.
+            Time.timeScale = 0.5f;
+        }
+
+        // 예를 들어, 다른 키를 눌러 다시 정상 속도로 돌릴 수 있습니다.
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // 시간을 정상 속도로 되돌립니다.
+            Time.timeScale = 1f;
+        }
+    }
+}
+*/
