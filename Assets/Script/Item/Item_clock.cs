28b752f293f2d1d20e18395655a12df16f8784f5
keonhwa <jk41587230@gmail.com>
2024-01-17 19:08:39 +0900

    1/17 아이템 시계 제작
    아이템 시계 제작

--- Assets/Script/Item/Item_clock.cs
+++ Assets/Script/Item/Item_clock.cs
@@ -2,38 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item_clock : MonoBehaviour
+public class TimeControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float originalTime;
+    private bool Slowed = false;
+
+    private void Start()
     {
-        
+        originalTime = Time.timeScale;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-    }
-}
-
-/*public class TimeControl : MonoBehaviour
-{
-    void Update()
-    {
-        // 어떤 조건에 따라 시간을 느리게 할 수 있습니다.
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Y))
         {
-            // 시간을 절반으로 줄입니다.
-            Time.timeScale = 0.5f;
-        }
-
-        // 예를 들어, 다른 키를 눌러 다시 정상 속도로 돌릴 수 있습니다.
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            // 시간을 정상 속도로 되돌립니다.
-            Time.timeScale = 1f;
+            if (Slowed == false)
+            {
+                Time.timeScale = 0.2f;
+                Slowed = true;
+                StartCoroutine(ReturnToOriginalTimeScale());
+            }
         }
     }
+
+    private IEnumerator ReturnToOriginalTimeScale()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Time.timeScale = originalTime;
+        Slowed = false;
+    }
 }
-*/
