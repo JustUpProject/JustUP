337c0144b318fb7f3f78830edc3b4d6a56f9775d
backsani <118903562+backsani@users.noreply.github.com>
2024-02-18 04:49:27 +0900

    2/18 4:49

--- Assets/Script/Monster/FireBall1_Left.cs
+++ Assets/Script/Monster/FireBall1_Left.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
-public class FireBall : MonoBehaviour
+public class FireBall_Left : MonoBehaviour
 {
     BasicControler player;
-    chapter_monster5 monster;
+    chapter_monster5_Left monster_left;
     Rigidbody2D fireBallRig;
     [SerializeField] private float speed;
     private bool direction; // right == 1, left == 0
@@ -15,10 +15,9 @@ public class FireBall : MonoBehaviour
     void Start()
     {
         player = FindObjectOfType<BasicControler>();
-        monster = FindObjectOfType<chapter_monster5>();
+        monster_left = FindObjectOfType<chapter_monster5_Left>();
         fireBallRig = FindObjectOfType<Rigidbody2D>();
         fireBallRig.gravityScale = 0f;
-        direction = monster.DirectionRight;
     }
 
     // Update is called once per frame
@@ -42,15 +41,9 @@ public class FireBall : MonoBehaviour
 
     private void Move()
     {
-        if (direction)
-        {
-            transform.position += new Vector3(Time.deltaTime * speed * -1, 0, 0);
+        
+        transform.position += new Vector3(Time.deltaTime * speed * -1, 0, 0);
 
-        }
-        else
-        {
-            transform.rotation = new Quaternion(0, 180, 0, 0);
-            transform.position += new Vector3(Time.deltaTime * speed, 0, 0);
-        }
+        
     }
 }
---
+++ Assets/Script/Monster/FireBall_Right.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class FireBall_Right : MonoBehaviour
+{
+    BasicControler player;
+    chapter_monster5_Right monster_right;
+    Rigidbody2D fireBallRig;
+    [SerializeField] private float speed;
+    private bool direction; // right == 1, left == 0
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<BasicControler>();
+        monster_right = FindObjectOfType<chapter_monster5_Right>();
+        fireBallRig = FindObjectOfType<Rigidbody2D>();
+        fireBallRig.gravityScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            player.PlayerHit();
+            Destroy(gameObject);
+        }
+        if (collision.collider.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Move()
+    {
+        
+        transform.rotation = new Quaternion(0, 180, 0, 0);
+        transform.position += new Vector3(Time.deltaTime * speed, 0, 0);
+        
+    }
+}
