405b8cb6be2406aa04bd7e7b8d5a8ca6eabc74e6
saper0w0 <100905653+saper0w0@users.noreply.github.com>
2024-02-18 18:36:37 +0900

    02/18
    오브젝트 아이스 중

---
+++ Assets/HSJ/objectIce.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class objectIce : MonoBehaviour
+{
+    [SerializeField] LayerMask Player;
+
+    private float originSlide;
+    private float originMoveSpeed;
+    private float changeMoveSpeed;
+
+    private void Start()
+    {
+        originSlide = BasicControler.Instance.slidingSpeed;
+        originMoveSpeed = BasicControler.Instance.moveSpeed;
+        changeMoveSpeed = BasicControler.Instance.moveSpeed*1.3f;
+    }
+
+    private void Update()
+    {
+        Vector2 vecL = this.transform.position;
+        Vector2 directionL = Vector2.left;
+
+        RaycastHit2D[] hirs = Physics2D.BoxCastAll(vecL, directionL);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+           if (BasicControler.Instance.ObjectCheck() == 2)
+            {
+                BasicControler.Instance.moveSpeed =changeMoveSpeed;
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            BasicControler.Instance.moveSpeed = originMoveSpeed;
+        }
+    }
+}
