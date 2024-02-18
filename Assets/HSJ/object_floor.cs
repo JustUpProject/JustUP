93d796ce2991384b1329ed7eaa5769d8a7280678
backsani <118903562+backsani@users.noreply.github.com>
2024-02-18 16:38:11 +0900

    2/18 4:38

--- Assets/HSJ/object_floor.cs
+++ Assets/HSJ/object_floor.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Linq;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public class object_floor : MonoBehaviour
